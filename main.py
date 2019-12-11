892bc98c6a084f33defd0a35a4d02b491d0b9e17
Chun Wai Lau, David <28540638+davidlaucw@users.noreply.github.com>
2019-12-11 11:22:20 +0100

    detailed specification
    make the truck specifiication more in detail

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import csv
 from datetime import datetime as dt
+import re
 
 logs = []
 fleet=[]
@@ -11,21 +12,64 @@ def start():
     message = "Hello. I am your truck chatbox."
     print(message)
     log(message)
+def isNumber(s):
+    try:
+        float(s)
+        return float(s)
+    except:
+        return None
 
 start()
 i = 0
 while True:
-    messageNewTruck = 'Adding the truck {} in your fleet, please input the specifications, or enter "stop" to stop:'.format(i+1)
+    messageNewTruck = 'Do you want to add the truck {} in your fleet? (y/n)'.format(i+1)
     newTruck = input(messageNewTruck)
     log(messageNewTruck)
     log(newTruck)
-    if newTruck == 'stop':
+    if newTruck == 'y':
+        messageAxle = 'What is the axle configuration? (nxn)'
+        axle = input(messageAxle)
+        log(messageAxle)
+        log(axle)
+        while (re.match('^[468]x[24]$',axle) or re.match('^10x[24]$',axle)) is None:
+            messageNxN = 'Please enter the correct axle configuration.'
+            axle = input(messageNxN)
+            log(messageNxN)
+            log(axle)
+            
+        messageWeight = 'What is the net weight of the truck in kg?'
+        weight = input(messageWeight)
+        log(messageWeight)
+        log(weight)
+        weight = isNumber(weight)
+        while weight is None:
+            messageNumberWeight = 'Please enter a number for the weight in kg.'
+            weight = input(messageNumberWeight)
+            log(messageNumberWeight)
+            log(weight)
+            weight = isNumber(weight)
+        
+        messageLoad = 'What is the load of the truck in kg?'
+        load = input(messageLoad)
+        log(messageLoad)
+        log(load)
+        load = isNumber(load)
+        while load is None:
+            messageNumberLoad = 'Please enter a number for the load in kg.'
+            weight = input(messageNumberLoad)
+            log(messageNumberLoad)
+            log(load)
+            load = isNumber(load)    
+        fleet.append([i+1,axle,weight,load])
+        i+=1
+    elif newTruck == 'n':
         break
     else:
-        fleet.append([i+1,newTruck])
-    i+=1
-
-messageEND = 'Your fleet of trucks is listed below (number,spec):'
+        messageyorn = 'Please enter y or n.'
+        print(messageyorn)
+        log(messageyorn)
+        
+messageEND = 'Your fleet of trucks is listed below (number,axle configuration,weight,load):'
 print(messageEND)
 log(messageEND)
 print(fleet)
