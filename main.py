9f1e48fd9ab887a2c83611c7cf5a926e91b35c92
Chun Wai Lau, David <28540638+davidlaucw@users.noreply.github.com>
2019-12-11 11:47:33 +0100

    improved README and chatlog
    Describe the program in more detail in README.
    Specifiy the side who send the messages  in the chat logs.

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import re
 
 logs = []
 fleet=[]
-def log(message):
-    logs.append([dt.now().strftime('%Y-%m-%d %H:%M:%S'),message])
+def log(side,message):
+    logs.append([dt.now().strftime('%Y-%m-%d %H:%M:%S'),side,message])
 def start():
     message = "Hello. I am your truck chatbox."
     print(message)
-    log(message)
+    log('bot',message)
 def isNumber(s):
     try:
         float(s)
@@ -22,60 +22,60 @@ def isNumber(s):
 start()
 i = 0
 while True:
-    messageNewTruck = 'Do you want to add the truck {} in your fleet? (y/n)'.format(i+1)
+    messageNewTruck = 'Do you want to add the truck {} in your fleet(y/n)? '.format(i+1)
     newTruck = input(messageNewTruck)
-    log(messageNewTruck)
-    log(newTruck)
+    log('bot',messageNewTruck)
+    log('user',newTruck)
     if newTruck == 'y':
-        messageAxle = 'What is the axle configuration? (nxn)'
+        messageAxle = 'What is the axle configuration (nxn)? '
         axle = input(messageAxle)
-        log(messageAxle)
-        log(axle)
+        log('bot',messageAxle)
+        log('user',axle)
         while (re.match('^[468]x[24]$',axle) or re.match('^10x[24]$',axle)) is None:
-            messageNxN = 'Please enter the correct axle configuration.'
+            messageNxN = 'Please enter the correct axle configuration. '
             axle = input(messageNxN)
-            log(messageNxN)
-            log(axle)
+            log('bot',messageNxN)
+            log('user',axle)
             
-        messageWeight = 'What is the net weight of the truck in kg?'
+        messageWeight = 'What is the net weight of the truck in kg? '
         weight = input(messageWeight)
-        log(messageWeight)
-        log(weight)
+        log('bot',messageWeight)
+        log('user',weight)
         weight = isNumber(weight)
         while weight is None:
-            messageNumberWeight = 'Please enter a number for the weight in kg.'
+            messageNumberWeight = 'Please enter a number for the weight in kg. '
             weight = input(messageNumberWeight)
-            log(messageNumberWeight)
-            log(weight)
+            log('bot',messageNumberWeight)
+            log('user',weight)
             weight = isNumber(weight)
         
-        messageLoad = 'What is the load of the truck in kg?'
+        messageLoad = 'What is the load of the truck in kg? '
         load = input(messageLoad)
-        log(messageLoad)
-        log(load)
+        log('bot',messageLoad)
+        log('user',load)
         load = isNumber(load)
         while load is None:
-            messageNumberLoad = 'Please enter a number for the load in kg.'
+            messageNumberLoad = 'Please enter a number for the load in kg. '
             weight = input(messageNumberLoad)
-            log(messageNumberLoad)
-            log(load)
+            log('bot',messageNumberLoad)
+            log('user',load)
             load = isNumber(load)    
         fleet.append([i+1,axle,weight,load])
         i+=1
     elif newTruck == 'n':
         break
     else:
-        messageyorn = 'Please enter y or n.'
+        messageyorn = 'Please enter y or n. '
         print(messageyorn)
-        log(messageyorn)
+        log('bot',messageyorn)
         
 messageEND = 'Your fleet of trucks is listed below (number,axle configuration,weight,load):'
 print(messageEND)
-log(messageEND)
+log('bot',messageEND)
 print(fleet)
-log(fleet)
+log('bot',fleet)
 
 #print(logs)
 
-df = pd.DataFrame(logs, columns= ['time', 'message'])
+df = pd.DataFrame(logs, columns= ['time','side', 'message'])
 df.to_csv(dt.now().strftime('%Y%m%d%H%M%S')+'.csv',index=False,quoting=csv.QUOTE_NONNUMERIC)
